aaeac04b386f6f87c68e721f72f6d8ec83a0dd5d
kceegram <158181757+kceegram@users.noreply.github.com>
2024-05-08 11:29:32 +0100

    aa

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='block lg:flex gap3'>
+    <div className='block lg:flex gap-3'>
        <h2 className='mb-3 lg:text-black text-white bg-blue-400 p-10'>This is Heading</h2>
        <h2 className='mb-3 lg:text-black text-white bg-blue-400 p-10'>This is Heading</h2>
     </div>
